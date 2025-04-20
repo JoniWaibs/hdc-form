0ec4ba1d1cb01c33fdfe6d3d159309965a2ca8e3
joniwaibs <joniwaibs@gmail.com>
2025-04-19 21:11:52 -0300

    feat: add error toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google"
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,6 +20,11 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+          <Toaster
+            position="top-center" 
+            closeButton
+            richColors
+          />
           {children}
         </ThemeProvider>
       </body>
