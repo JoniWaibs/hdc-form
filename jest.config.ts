0e929be916757a82c6ed1deae90f094777499d8f
joniwaibs <joniwaibs@gmail.com>
2025-04-27 10:54:41 -0300

    feat: add testing configuration

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+// jest.config.ts
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+  dir: './', // path a tu proyecto
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/lib/(.*)$': '<rootDir>/src/lib/$1',
+    '^@/services/(.*)$': '<rootDir>/src/services/$1',
+    '^@/app/(.*)$': '<rootDir>/src/app/$1',
+  },
+  testEnvironment: 'jsdom',
+  testMatch: [
+    '<rootDir>/__tests__/**/*.(spec|test).(ts|tsx)',
+  ],
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.jest.json',
+    },
+  },
+}
+
+export default createJestConfig(customJestConfig)
