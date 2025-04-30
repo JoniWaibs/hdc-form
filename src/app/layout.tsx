346681bddeff03bc3845b48bf315b52361616d86
joniwaibs <joniwaibs@gmail.com>
2025-04-30 14:24:22 -0300

    feat: add new favicon + manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,9 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Hablemos de cancer",
   description: "Plataforma de formaciones",
+  manifest: "/site.webmanifest",
   icons: {
-    icon: '/favicon.png',
+    icon: '/favicon.ico',
   },
 };
 
